3ac7f8f05e8ea254362554f2dcbc144855ebd7df
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-08 13:43:59 +0100

    Ajout des models

---
+++ models/activite.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const activiteSchema = new Schema({
+    activity: {
+        type: String,
+        required: true
+    },
+    accessibility: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    participants: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    key: {
+        type: String,
+        required: true
+    },
+
+})
+
+
+module.exports = mongoose.model("Activite", activiteSchema)
