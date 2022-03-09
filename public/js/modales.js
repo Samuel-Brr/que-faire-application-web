c1d191088cbf90a775a61d93bd281d691dfb4a57
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-09 12:49:50 +0100

    hotfix modale

--- public/js/modales.js
+++ public/js/modales.js
@@ -1,25 +1,29 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+const openModal = function(myBtn) {
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+    // Get the modal
+    const activiteId = myBtn.parentNode.querySelector('[name=activiteId]').value
+    const modal = document.getElementById(`myModal-${activiteId}`);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    // Get the button that opens the modal
+    const btn = document.getElementById(`myBtn-${activiteId}`);
+    
+
+    // Opens modal
+    modal.style.display = "block";
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 }
